cac9800543b43e976069d2bf08f4d10a83142acd
KaNicoleti <125992086+KaNicoleti@users.noreply.github.com>
2023-08-30 00:26:59 -0300

    atualizado

--- src/app/acessorios/acessorios.page.ts
+++ src/app/acessorios/acessorios.page.ts
@@ -12,30 +12,31 @@ export class AcessoriosPage implements OnInit {
 
   listaProdutos = [
     {
-      nome: "alfinetes",
-      descricao: "50 unidades",
-      valor: 10,
+      nome: "Brico Macramê",
+      descricao: "2.00cm - emborrachada",
+      valor: 100,
       // por o endereço da foto com o caminho//
-     foto:''
+     foto:'https://i.pinimg.com/736x/0f/15/e7/0f15e764a49ba79ba3ea86b0fc5773d5.jpg'
     },
     {
-      nome: "Marcador de pontos",
-      descricao: "10 unidades",
+      nome: "Tapete de banheiro (jogo)",
+      descricao: "3 peças",
       valor: 5,
-      foto:''
+      foto:'https://i.pinimg.com/564x/d1/b1/f4/d1b1f4d8b691589352bdb166a5ef3422.jpg'
     },
     {
-      nome: "olhos achatados 10 pares",
-      descricao: "9cm - redondo",
-      valor: 15,
-      foto:''
+      nome: "Capa de banco",
+      descricao: "Flor",
+      valor: 25,
+      foto:'https://i.pinimg.com/736x/c8/a5/7f/c8a57ff4c2fe8e35a24a414d73a78c52.jpg'
     },
     {
-      nome: "olhos pinos 10 pares",
-      descricao: "9cm - redondo",
-      valor: 15,
-      foto:''
+      nome: "Tapete de banheiro azul",
+      descricao: "3 peças",
+      valor: 25,
+      foto:'https://i.pinimg.com/564x/00/a9/fb/00a9fb0c039ec4e2541b3879f71397a2.jpg'
     },
+   
   ];
   constructor(public bdtemp: BdtempService) { }
 
@@ -51,3 +52,4 @@ export class AcessoriosPage implements OnInit {
     this.qtdeItensCarrinho = this.bdtemp.buscar('qtdeItensCarrinho');
   }
 }
+
--- src/app/linhas/linhas.page.ts
+++ src/app/linhas/linhas.page.ts
@@ -12,29 +12,106 @@ export class LinhasPage implements OnInit {
 
   listaProdutos = [
     {
-      nome: "Linha Merce",
+      nome: "Linha Amazônia",
       descricao: "3.00mts - algodão",
       valor: 19.98,
       // por o endereço da foto com o caminho//
-     foto:''
+     foto:'https://www.armarinhosaojose.com.br/octopus/design/images/94/products/b/barbante-amazonia-4-600g-921m.jpg'
     },
     {
-      nome: "Agulha de tapeceiro",
+      nome: "Linha Floe",
       descricao: "3.00mts - algodão",
       valor: 5,
-      foto:''
+      foto:'https://i.pinimg.com/564x/f4/c6/e9/f4c6e95d3d45ed0e9a0be256a160f9c1.jpg'
     },
     {
-      nome: "linha euroroma",
+      nome: "linha Pasione",
       descricao: "3.00mts - algodão",
       valor: 25,
-      foto:''
+      foto:'https://www.armarinhosaojose.com.br/octopus/design/images/94/products/b/linha-passione.jpg'
     },
     {
-      nome: "olhos pinos 10 pares",
+      nome: "Linha Euroroma",
       descricao: "3.00mts - algodão",
       valor: 25,
-      foto:'https://img.elo7.com.br/product/zoom/3562857/5-pares-olhos-15-mm-castanhos-boneca-esperanca-olhos-castanhos.jpg'
+      foto:'https://images.tcdn.com.br/img/img_prod/605770/barbante_euroroma_colorido_n6_1kg_cor_900_azul_bebe_4320_1_ab70b240c7f4666a88410d9f4614c91d.jpg'
+    },
+    {
+      nome: "Linha Euroroma",
+      descricao: "3.00mts - algodão",
+      valor: 25,
+      foto:'https://i.pinimg.com/originals/11/4c/31/114c3179e5eb3a2b3d33202b1c0ad045.jpg'
+    },
+    {
+      nome: "Linha Euroroma",
+      descricao: "3.00mts - algodão",
+      valor: 25,
+      foto:'https://www.armarinhosaojose.com.br/octopus/design/images/94/products/b/barbante-roma-6-1kg.jpg'
+    },
+  ];
+
+  constructor(public bdtemp: BdtempService) { }
+
+  ngOnInit() {
+  }
+  addProdutoCarrinho(produto: any){
+    this.bdtemp.addProdutoCarrinho(produto);
+
+    this.buscarDadosCarrinho();
+  }
+
+  buscarDadosCarrinho(){
+    this.qtdeItensCarrinho = this.bdtemp.buscar('qtdeItensCarrinho');
+  }
+}import { Component, OnInit } from '@angular/core';
+import { BdtempService } from '../services/bdtemp.service';
+
+@Component({
+  selector: 'app-linhas',
+  templateUrl: './linhas.page.html',
+  styleUrls: ['./linhas.page.scss'],
+})
+export class LinhasPage implements OnInit {
+
+  qtdeItensCarrinho = 0;
+
+  listaProdutos = [
+    {
+      nome: "Linha Amazônia",
+      descricao: "3.00mts - algodão",
+      valor: 19.98,
+      // por o endereço da foto com o caminho//
+     foto:'https://www.armarinhosaojose.com.br/octopus/design/images/94/products/b/barbante-amazonia-4-600g-921m.jpg'
+    },
+    {
+      nome: "Linha Floe",
+      descricao: "3.00mts - algodão",
+      valor: 5,
+      foto:'https://i.pinimg.com/564x/f4/c6/e9/f4c6e95d3d45ed0e9a0be256a160f9c1.jpg'
+    },
+    {
+      nome: "linha Pasione",
+      descricao: "3.00mts - algodão",
+      valor: 25,
+      foto:'https://www.armarinhosaojose.com.br/octopus/design/images/94/products/b/linha-passione.jpg'
+    },
+    {
+      nome: "Linha Euroroma",
+      descricao: "3.00mts - algodão",
+      valor: 25,
+      foto:'https://images.tcdn.com.br/img/img_prod/605770/barbante_euroroma_colorido_n6_1kg_cor_900_azul_bebe_4320_1_ab70b240c7f4666a88410d9f4614c91d.jpg'
+    },
+    {
+      nome: "Linha Euroroma",
+      descricao: "3.00mts - algodão",
+      valor: 25,
+      foto:'https://i.pinimg.com/originals/11/4c/31/114c3179e5eb3a2b3d33202b1c0ad045.jpg'
+    },
+    {
+      nome: "Linha Euroroma",
+      descricao: "3.00mts - algodão",
+      valor: 25,
+      foto:'https://www.armarinhosaojose.com.br/octopus/design/images/94/products/b/barbante-roma-6-1kg.jpg'
     },
   ];
 
